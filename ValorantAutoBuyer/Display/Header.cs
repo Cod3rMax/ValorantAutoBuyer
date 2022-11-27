855a77ed746d0714b574ef4939fda0adc2fea5f6
Cod3rMax <dzxxxw@gmail.com>
2022-11-27 16:38:43 +0100

    Add Display Header to the project

---
+++ ValorantAutoBuyer/Display/Header.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Drawing;
+using Console = Colorful.Console;
+
+namespace ValorantAutoBuyer.Display
+{
+    public class Header
+    {
+        private static readonly string Line1 = " █████╗ ██╗   ██╗████████╗ ██████╗ ██████╗ ██╗   ██╗██╗   ██╗███████╗██████╗";
+        private static readonly string Line2 = " ██╔══██╗██║   ██║╚══██╔══╝██╔═══██╗██╔══██╗██║   ██║╚██╗ ██╔╝██╔════╝██╔══██╗";
+        private static readonly string Line3 = " ███████║██║   ██║   ██║   ██║   ██║██████╔╝██║   ██║ ╚████╔╝ █████╗  ██████╔╝";
+        private static readonly string Line4 = " ██╔══██║██║   ██║   ██║   ██║   ██║██╔══██╗██║   ██║  ╚██╔╝  ██╔══╝  ██╔══██╗";
+        private static readonly string Line5 = " ██║  ██║╚██████╔╝   ██║   ╚██████╔╝██████╔╝╚██████╔╝   ██║   ███████╗██║  ██║";
+        private static readonly string Line6 = " ╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝ ╚═════╝  ╚═════╝    ╚═╝   ╚══════╝╚═╝  ╚═╝";
+        private static readonly string Line7 = "~ Made by Cod3rMax ~";
+        private static readonly string Line8 = @"{ Discord: https://discord.gg/Dxh3yY3TqA }";
+        public static void ValorantAutoBuyerHeader()
+        {
+            Console.WindowWidth = 130;
+            Console.WindowHeight = 35;
+
+            Console.WriteLine();
+
+            Console.SetCursorPosition((int)Math.Round((Console.WindowWidth - Line1.Length) / 2.0),
+                Console.CursorTop);
+            Console.WriteLine(Line1, Color.Red);
+
+            Console.SetCursorPosition((int)Math.Round((Console.WindowWidth - Line2.Length) / 2.0),
+                Console.CursorTop);
+            Console.WriteLine(Line2, Color.Red);
+
+            Console.SetCursorPosition((int)Math.Round((Console.WindowWidth - Line3.Length) / 2.0),
+                Console.CursorTop);
+            Console.WriteLine(Line3, Color.Red);
+
+            Console.SetCursorPosition((int)Math.Round((Console.WindowWidth - Line4.Length) / 2.0),
+                Console.CursorTop);
+            Console.WriteLine(Line4, Color.Red);
+
+            Console.SetCursorPosition((int)Math.Round((Console.WindowWidth - Line5.Length) / 2.0),
+                Console.CursorTop);
+            Console.WriteLine(Line5, Color.Red);
+
+            Console.SetCursorPosition((int)Math.Round((Console.WindowWidth - Line6.Length) / 2.0),
+                Console.CursorTop);
+            Console.WriteLine(Line6, Color.Red);
+
+            Console.WriteLine();
+
+            Console.SetCursorPosition((int)Math.Round((Console.WindowWidth - Line7.Length) / 2.0),
+                Console.CursorTop);
+            Console.WriteLine(Line7, Color.GreenYellow);
+
+            Console.SetCursorPosition((int)Math.Round((Console.WindowWidth - Line8.Length) / 2.0),
+                Console.CursorTop);
+            Console.WriteLine(Line8, Color.Aqua);
+
+            Console.WriteLine();
+
+            Console.Write("[F1] => ", Color.GreenYellow);
+            Console.Write("Purchase All Abilities only.", Color.BurlyWood);
+            Console.WriteLine();
+
+            Console.Write("[F2] => ", Color.GreenYellow);
+            Console.Write("Purchase Spectre, Light Shield and the rest of money get All Abilities.", Color.BurlyWood);
+            Console.WriteLine();
+
+            Console.Write("[F3] => ", Color.GreenYellow);
+            Console.Write("Purchase Bulldog, Light Shield and the rest of money get All Abilities.", Color.BurlyWood); Console.WriteLine();
+
+            Console.Write("[F5] => ", Color.GreenYellow);
+            Console.Write("Purchase Vandal, Heavy Shield and All Abilities.", Color.BurlyWood);
+            Console.WriteLine();
+
+            Console.Write("[F6] => ", Color.GreenYellow);
+            Console.Write("Purchase Phantom, Heavy Shield and All Abilities.", Color.BurlyWood);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("[*] => Choose your purchase in game:", Color.BurlyWood);
+        }
+    }
+}
--- ValorantAutoBuyer/Program.cs
+++ ValorantAutoBuyer/Program.cs
@@ -15,6 +15,7 @@ namespace ValorantAutoBuyer
 
         public static void Main(string[] args)
         {
+            Display.Header.ValorantAutoBuyerHeader();
             var hWind = Helper.ValorantWindow.FindWindow(IntPtr.Zero, "VALORANT  ");
             Helper.ValorantWindow.GetWindowRect(hWind, out Helper.ValorantWindow._rect);
             
